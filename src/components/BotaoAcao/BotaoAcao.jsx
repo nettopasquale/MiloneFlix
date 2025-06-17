3ab45b5a5e52b46631ab4cb19b9e625a34a42dc9
Pasquale <pasqualenetto@yahoo.com.br>
2025-06-17 18:54:17 -0300

    projeto Programação Web EM CONSTRUÇÃO - Versao para subir no Vercel

---
+++ src/components/BotaoAcao/BotaoAcao.jsx
@@ -0,0 +1,25 @@
+import {Link} from 'react-router-dom';
+
+export default function BotaoAcao({
+    children,
+    onClick,
+    to }) {
+    const baseClass =
+    "px-40 py-4 mt-6 text-3xl font-semibold text-white bg-green-700 rounded-lg hover:bg-green-800 transition duration-200";
+  
+  if (to) {
+    return (
+      <Link to={to} className={baseClass}>
+        {children}
+      </Link>
+    );
+  }
+
+  return (
+    <button onClick={onClick} className={baseClass}>
+      {children}
+    </button>
+  );
+  
+
+};
